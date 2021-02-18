69a291b08c15bfefe2363cde91eb9b111586a14f
Ryan Vitter <ryanvitter@gmail.com>
2021-02-18 11:53:15 -0500

    add collection page

---
+++ client/pages/_collection.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container mx-auto">
+    <category
+      v-for="category in categories"
+      :key="category.id"
+      :category="category"
+      :resources="resources.filter(r => r.categories.map(c => c.id).includes(category.id))">
+    </category>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      categories: [],
+      resources: []
+    }
+  },
+  created: async function () {
+
+    const categories = await fetch(`${process.env.apiUrl}/categories`)
+    this.categories = await categories.json()
+    const resources = await fetch(`${process.env.apiUrl}/resources`)
+    this.resources = await resources.json()
+  }
+}
+</script>
