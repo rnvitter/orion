3dccc03818a45158376272af6b461818b98e66fc
Ryan Vitter <ryanvitter@gmail.com>
2021-02-18 20:14:53 -0500

    add config for tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 200ms ease-in forwards'
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        }
+      }
+    },
+  },
+  plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+      // TypeScript
+      'plugins/**/*.ts',
+      'nuxt.config.ts'
+    ]
+  },
+  variants: {
+    animation: ['motion-safe']
+  }
+}
